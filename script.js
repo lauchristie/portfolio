ae46da501408210386a1afedc140c7e056bfa721
lauchristie <68232190+lauchristie@users.noreply.github.com>
2025-10-01 10:48:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ function createASCIILogo() {
     
     // Exported values from debug session - for STL file
     const stlValues = {
-        camera: { x: 0, y: 20, z: 40 },
+        camera: { x: 0, y: 0, z: 40 },
         scale: 0.7,
         rotation: { x: -120, y: -5, z: -100 },
         position: { x: 0, y: 0, z: 0 },
